76270df4a88d4f846f68daf7bb9ab1a53bbaea55
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-07-06 17:22:37 +0300

    cors error fixed

--- functions/2-basic-api.js
+++ functions/2-basic-api.js
@@ -2,6 +2,9 @@ const items = require('../assets/data');
 
 exports.handler = async (event, context, cb) => {
   return {
+    Headers: {
+      'Access-Control-Allow-Origin': '*'
+    },
     statusCode: 200,
     body: JSON.stringify(items)
   }
