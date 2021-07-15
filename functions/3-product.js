ec6629f7cd4318a8acc276cb50d3a556a3c18185
Lauri Kyttälä <lauri.kyttala@gmail.com>
2021-07-15 13:02:21 +0300

    Single Product Function

---
+++ functions/3-product.js
@@ -0,0 +1,36 @@
+require('dotenv').config()
+const Airtable = require('airtable-node');
+
+const airtable = new Airtable({ apiKey: process.env.AIRTABLE_API_KEY })
+  .base('applJ8bM3FAKWP3TH')
+  .table('products')
+
+exports.handler = async (event, context, cb) => {
+  // console.log(event)
+  const { id } = event.queryStringParameters
+  if (id) {
+    try {
+      const product = await airtable.retrieve(id);
+      // console.log(product)
+      if (product.error) {
+        return {
+          statusCode: 404,
+          body: `No product with id: ${id}`
+        }
+      }
+      return {
+        statusCode: 200,
+        body: JSON.stringify(product)
+      }
+    } catch (error) {
+      return {
+        statusCode: 500,
+        body: 'Server error'
+      }
+    }
+  }
+  return {
+    statusCode: 400,
+    body: 'Please provide product id',
+  }
+}
